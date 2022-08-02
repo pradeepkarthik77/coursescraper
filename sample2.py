798f2d0631504cf7a2dd0993e7ea3c122eecf7c9
pradeepkarthik77 <pradeepkarthikm@gmail.com>
2022-08-02 19:55:46 +0530

    program whivh is pre stage to data collection. this can collect the pages using json.loads and store the 5 page objects in a list for future usage.

--- sample2.py
+++ sample2.py
@@ -4,33 +4,51 @@ import urllib
 import json
 import time
 
-algolia_url = "https://lua9b20g37-dsn.algolia.net/1/indexes/test_products?x-algolia-application-id=LUA9B20G37&x-algolia-api-key=dcc55281ffd7ba6f24c3a9b18288499b&hitsPerPage=1000&page="
 
-#with urllib.request.urlopen(f'{algolia_url}') as url:
 
-nbhits = 1000
 
-pageiter = 1
 
-while nbhits!=0:
 
-    url = algolia_url+str(pageiter)
+def maincontrol():
 
-    pageiter+=1
+    algolia_url = "https://lua9b20g37-dsn.algolia.net/1/indexes/test_products?x-algolia-application-id=LUA9B20G37&x-algolia-api-key=dcc55281ffd7ba6f24c3a9b18288499b&hitsPerPage=1000&page="
 
-    pageurl = urllib.request.urlopen(url)
+    #with urllib.request.urlopen(f'{algolia_url}') as url:
+
+    nbhits = 11000 #initialising the nbhits parameter inorder to make sure that initially page is going inside while loop
+
+    pageiter = 1 #initial page is 1
+
+    json_objects = []
+
+    while nbhits!=0: #goes on till empty page is read
+
+        url = algolia_url+str(pageiter) #add page number to url
+
+        pageurl = urllib.request.urlopen(url)
+        
+        print(f"Fetching coursera program data on page {pageiter}.")
+        
+        page_data = json.loads(pageurl.read().decode())
+
+
+        jsonobj = json.dumps(page_data,indent=4,sort_keys=True) #write
+        myfile = open('samplefile.txt','a')                     #data to a file for backup
+        myfile.writelines(jsonobj)
+
+        #print(jsonobj)
+
+        json_objects.append(page_data)
+
+        nbhits = int(page_data['nbHits'])
+
+        print("Page :",pageiter,nbhits)
+        
+        #time.sleep(60) #wait for 1 min so no need to put extra load on server
+
+        pageiter+=1 #increment the page
+
     
-    print(f"Fetching coursera program data on page .")
-    
-    page_data = json.loads(pageurl.read().decode())
 
-    jsonobj = json.dumps(page_data,indent=4,sort_keys=True)
-
-    myfile = open('samplefile1.txt','w')
-    myfile.writelines(jsonobj)
-
-    #print(jsonobj)
-
-    nbhits = int(page_data['nbHits'])
-    print("Page :",pageiter,nbhits)
-    #time.sleep(30)
+if __name__ == "__main__":
+    maincontrol()
