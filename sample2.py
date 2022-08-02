6db7f18ebdb8e7257b1981655701655d546a2b83
pradeepkarthik77 <pradeepkarthikm@gmail.com>
2022-08-02 19:39:42 +0530

    scraping coursing from web done

--- sample2.py
+++ sample2.py
@@ -2,16 +2,35 @@ from bs4 import BeautifulSoup
 import requests
 import urllib
 import json
+import time
 
-algolia_url = "https://lua9b20g37-dsn.algolia.net/1/indexes/test_products?x-algolia-application-id=LUA9B20G37&x-algolia-api-key=dcc55281ffd7ba6f24c3a9b18288499b&hitsPerPage=1000&page=1"
+algolia_url = "https://lua9b20g37-dsn.algolia.net/1/indexes/test_products?x-algolia-application-id=LUA9B20G37&x-algolia-api-key=dcc55281ffd7ba6f24c3a9b18288499b&hitsPerPage=1000&page="
 
-with urllib.request.urlopen(f'{algolia_url}') as url:
+#with urllib.request.urlopen(f'{algolia_url}') as url:
 
+nbhits = 1000
+
+pageiter = 1
+
+while nbhits!=0:
+
+    url = algolia_url+str(pageiter)
+
+    pageiter+=1
+
+    pageurl = urllib.request.urlopen(url)
     
     print(f"Fetching coursera program data on page .")
-    page_data = json.loads(url.read().decode())
+    
+    page_data = json.loads(pageurl.read().decode())
 
     jsonobj = json.dumps(page_data,indent=4,sort_keys=True)
 
-    myfile = open('samplefile.txt','w')
+    myfile = open('samplefile1.txt','w')
     myfile.writelines(jsonobj)
+
+    #print(jsonobj)
+
+    nbhits = int(page_data['nbHits'])
+    print("Page :",pageiter,nbhits)
+    #time.sleep(30)
